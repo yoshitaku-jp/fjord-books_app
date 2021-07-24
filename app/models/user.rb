b1753ffbe79d6e1ff4ec61a71cf133f98aefaddb
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-07-24 14:00:44 +0900

    バリデーション設定

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :postalcode, :address, presence: true
+  validates :selfintroduction, length: { maximum: 200 }
+end
