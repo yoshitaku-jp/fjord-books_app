e3616cc8faa67f915b7752b5df57794bed399559
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-08-11 07:34:38 +0900

    更新時のStrong Parameter追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[postal_code address selfintroduction])
   end
 
   def after_sign_out_path_for(_resource)
