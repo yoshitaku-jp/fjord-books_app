0fb7928beffd11d222af92036ad4a3c0ea27b1d8
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-08-10 22:07:13 +0900

    confirmableの機能を追加

--- db/migrate/20210723220234_add_postalcode_adress_selfintroduction_to_users.rb
+++ db/migrate/20210723220234_add_postalcode_adress_selfintroduction_to_users.rb
@@ -1,6 +1,6 @@
 class AddPostalcodeAdressSelfintroductionToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :postalcode, :integer
+    add_column :users, :postal_code, :integer
     add_column :users, :address, :string
     add_column :users, :selfintroduction, :text
   end
