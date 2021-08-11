c306d69825506c04c47dcd53a7f3b608d4f57190
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-08-11 21:58:42 +0900

    UserとFollowRelationshipの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+  has_many :followers, class_name: 'FollowRelationship',
+                       foreign_key: 'follower_id',
+                       dependent: :destroy
+  has_many :followings, class_name: 'FollowRelationship',
+                        foreign_key: 'followee_id',
+                        dependent: :destroy
 end
