b9b00b3ef76585a5067010fa0c086b82acc10fdd
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-08-11 21:49:13 +0900

    rails generate model FollowRelationship を実行

---
+++ app/models/follow_relationship.rb
@@ -0,0 +1,2 @@
+class FollowRelationship < ApplicationRecord
+end
---
+++ db/migrate/20210811124428_create_follow_relationships.rb
@@ -0,0 +1,10 @@
+class CreateFollowRelationships < ActiveRecord::Migration[6.1]
+  def change
+    create_table :follow_relationships do |t|
+      t.integer :follower_id
+      t.integer :followee_id
+
+      t.timestamps
+    end
+  end
+end
