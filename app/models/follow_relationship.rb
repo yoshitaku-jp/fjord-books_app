cee8c68d8aeb7faed66bf27cce8eb9cb47e4e5d8
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-08-15 08:27:49 +0900

    follow機能動作確認

--- app/models/follow_relationship.rb
+++ app/models/follow_relationship.rb
@@ -1,4 +1,4 @@
 class FollowRelationship < ApplicationRecord
   belongs_to :follower, class_name: 'User'
-  belongs_to :followee, class_name: 'User'
+  belongs_to :followed, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
-  has_many :followers, class_name: 'FollowRelationship',
-                       foreign_key: 'follower_id',
-                       dependent: :destroy
-  has_many :followings, class_name: 'FollowRelationship',
-                        foreign_key: 'followee_id',
-                        dependent: :destroy
+  has_many :active_relationships, class_name: 'FollowRelationship',
+                                  foreign_key: 'follower_id',
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
