99e0ae830587e4bd80dcc14e3f295de4839a1e8c
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-08-15 14:02:33 +0900

    selfintroductionをself_introductionに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[postal_code address selfintroduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[postal_code address self_introduction])
   end
 
   def after_sign_out_path_for(_resource)
