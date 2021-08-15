832e98f560274ae36c776f6a7d22ebf6e06e27dc
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-08-15 09:04:28 +0900

    フォロワー機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,11 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: 'FollowRelationship',
                                   foreign_key: 'follower_id',
                                   dependent: :destroy
+  has_many :passive_relationships, class_name: 'FollowRelationship',
+                                   foreign_key: 'followed_id',
+                                   dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   def follow(other_user)
     following << other_user
