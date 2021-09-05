f02617a64b38a4a4e8782e7197b98e4408ee3c38
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-09-05 23:04:00 +0900

    validatesを付与

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,4 +3,5 @@
 class Report < ApplicationRecord
   belongs_to :user, dependent: :destroy
   has_many :comments, as: :commentable, dependent: :destroy
+  validates :title,:content, presence: true
 end
