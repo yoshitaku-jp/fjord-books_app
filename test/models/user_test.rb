a0dc6ca6ca20938b40688d5f1b2a7cb61902ad14
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-09-07 08:23:23 +0900

    followテスト

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,4 +11,13 @@ class UserTest < ActiveSupport::TestCase
     user.name = 'Foo Bar'
     assert_equal 'Foo Bar', user.name_or_email
   end
+
+  test '#follow' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+
+    assert_not me.following?(she)
+    me.follow(she)
+    assert me.following?(she)
+  end
 end
