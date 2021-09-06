1ae2d14c6ba62d92a6b6939998ef3194595e1dbb
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-09-06 23:00:22 +0900

    インデントの修正

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 module CommentsHelper
-    def name_or_email(comment)
-        if comment.user.name? 
-            comment.user.name
-        else
-            comment.user.email
-        end
+  def name_or_email(comment)
+    if comment.user.name?
+      comment.user.name
+    else
+      comment.user.email
     end
+  end
 end
