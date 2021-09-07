31a9959daabd0ec53d77a38fb8f802c0805142bb
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-09-07 20:07:54 +0900

    created_onテスト

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,8 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "created_on" do
+    report = reports(:one)
+    assert_equal Date.today, report.created_on
+  end
 end
