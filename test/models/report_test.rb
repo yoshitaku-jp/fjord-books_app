b328dee4d46222a6a7da9e1acb175136a8587851
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-09-07 20:27:26 +0900

    editableテスト

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,6 +3,15 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
+  test "editable" do
+    report = reports(:one)
+    one = users(:one)
+    assert report.editable?(one)
+
+    alice = users(:alice)
+    assert_not report.editable?(alice)
+  end
+
   test "created_on" do
     report = reports(:one)
     assert_equal Date.today, report.created_on
