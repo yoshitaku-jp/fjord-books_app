9f73d089d6340de5efe98393a1d83fbf76ff7f26
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-09-09 08:04:11 +0900

    ローカル変数に変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,11 +5,11 @@ class CommentsController < ApplicationController
 
   def create
     resource, id = request.path.split('/')[1, 2]
-    @commentable = resource.singularize.classify.constantize.find(id)
-    @comment = @commentable.comments.new(comment_params.merge(user: current_user))
+    commentable = resource.singularize.classify.constantize.find(id)
+    comment = commentable.comments.new(comment_params.merge(user: current_user))
 
-    @comment.save!
-    redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human) 
+    comment.save!
+    redirect_to commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human) 
   end
 
   private
